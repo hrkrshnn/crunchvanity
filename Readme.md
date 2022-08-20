cb290ad458d4497732261fc86c485bf5357753bf
hrkrshnn <webmail.hari@gmail.com>
2022-08-20 12:59:12 +0200

    Something that works

---
+++ Readme.md
@@ -0,0 +1,7 @@
+A cruncher for Vanity.
+
+```
+cargo run --release
+```
+
+Runs in around 3 minutes on a 32 core machine.
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,43 @@
-use std::str::FromStr;
-
 use ethers::prelude::*;
 use ethers::abi::{AbiEncode};
 
 use eyre::Result;
 
+use sha256::{, digest_bytes};
+
+use rayon::prelude::*;
+
 abigen!(VanityContract, "./out/VanityContract.sol/VanityContract.json");
 
 fn main() -> Result<()> {
-    let hash: [u8; 32] =  hex::decode("19bb34e293bba96bf0caeea54cdd3d2dad7fdf44cbea855173fa84534fcfb528")?.try_into()?;
-    let x = IsValidSignatureCall::encode(
-        IsValidSignatureCall
-        {
-            hash,
-            signature: Bytes::from_str("hello").unwrap()
+    let hash: [u8; 32] = hex::decode("19bb34e293bba96bf0caeea54cdd3d2dad7fdf44cbea855173fa84534fcfb528")?.try_into().unwrap();
+    let selector: [u8; 4] = hex::decode("1626ba7e")?.try_into().unwrap();
+
+    let res = (0..u64::MAX).into_par_iter().
+        find_any(|i| {
+            let signature = format!("{}",i);
+            let signature: Vec<u8> = signature.as_bytes().into();
+            let signature = Bytes::from(signature);
+            let x = IsValidSignatureCall::encode(
+                IsValidSignatureCall
+                {
+                    hash,
+                    signature: signature.clone()
+                });
+
+            let digest = digest_bytes(&x);
+            if digest[..8] == String::from("1626ba7e") {
+                println!("i: {}", i);
+                println!("signature: {}", signature);
+                println!("digest: {}", digest);
+                true
+            }
+            else {
+                false
+            }
+
         });
-    println!("{:?}", x);
+
+    println!("{:?}", res);
     Ok(())
 }
